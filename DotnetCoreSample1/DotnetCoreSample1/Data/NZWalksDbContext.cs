162955a31225e3bf427023914b83c45bc3d68753
ginoabraham <abraham.gino@gmail.com>
2022-11-08 20:59:31 -0500

    part 2 commit

---
+++ DotnetCoreSample1/DotnetCoreSample1/Data/NZWalksDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DotnetCoreSample1.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetCoreSample1.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+            { }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
